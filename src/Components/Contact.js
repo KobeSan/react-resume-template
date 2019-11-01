0169306d7d0652637ecc8159808a4f2ade733c79
KobeSan <baptiste.gislot@gmail.com>
2019-11-01 15:33:18 +0100

    Changed contact form and made it work

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      contactSubject: '',
+      contactMessage: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
 
     if(this.props.data){
@@ -39,7 +53,7 @@ class Contact extends Component {
                <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
-                  <div>
+                  {/* <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
@@ -47,23 +61,23 @@ class Contact extends Component {
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
+                  </div> */}
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+						   <input type="text" value={this.state.subject} size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.message} onChange={this.handleChange}></textarea>
                   </div>
 
                   <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
+                     <a target='_blank' href={`mailto:baptistegislot@gmail.com?subject=${this.state.contactSubject}&body=${this.state.contactMessage}`}><button type='button' className="mailto">Submit</button></a>
+                     {/* <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
-                     </span>
+                     </span> */}
                   </div>
 					</fieldset>
 				   </form>
