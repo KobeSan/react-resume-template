f29cac7bcbdb5465a598b06a6efd26634703f989
KobeSan <baptiste.gislot@gmail.com>
2019-11-05 11:48:42 +0100

    added widl code school in education

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -23,47 +23,39 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-
-
-      <div className="row work">
-        <div className="three columns header-col">
-          <h1><span>Work</span></h1>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            {work}
+          </div>
         </div>
-        <div className="nine columns main-col">
-          {work}
+        <div className="row education">
+          <div className="three columns header-col">
+              <h1><span>Education</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">
+                  {education}
+                </div>
+              </div>
+          </div>
         </div>
-      </div>
-      
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
+        <div className="row skill">
+          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
+          </div>
+          <div className="nine columns main-col">
             <p>{skillmessage}
             </p>
-
-				<div className="skillz">
-					{skills}
-				</div>
-			</div>
-      </div>
+				    <div className="skillz">
+					    {skills}
+				    </div>
+			    </div>
+        </div>
    </section>
     );
   }
