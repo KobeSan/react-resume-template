e15770ffcd84864341f09de6852ba39c7f491f64
kobesan <baptiste.gislot@gmail.com>
2020-04-14 11:22:32 +0200

    added gitlab and refresh

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,62 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Resume extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
             <p>{work.description}</p>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-        return <img style={{width:skills.size, height:skills.size}} src={skills.url} alt={skills.name}></img>
-      })
+          </div>
+        );
+      });
+      var skills = this.props.data.skills.map(function (skills) {
+        return (
+          <img
+            style={{ width: skills.size, height: skills.size, margin: "5px" }}
+            src={skills.url}
+            alt={skills.name}
+          ></img>
+        );
+      });
     }
 
     return (
       <section id="resume">
         <div className="row work">
           <div className="three columns header-col">
-            <h1><span>Expériences</span></h1>
+            <h1>
+              <span>Expériences</span>
+            </h1>
           </div>
-          <div className="nine columns main-col">
-            {work}
-          </div>
+          <div className="nine columns main-col">{work}</div>
         </div>
         <div className="row education">
           <div className="three columns header-col">
-              <h1><span>Formations</span></h1>
+            <h1>
+              <span>Formations</span>
+            </h1>
           </div>
 
           <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">
-                  {education}
-                </div>
-              </div>
+            <div className="row item">
+              <div className="twelve columns">{education}</div>
+            </div>
           </div>
         </div>
         <div className="row skill">
           <div className="three columns header-col">
-            <h1><span>Compétences</span></h1>
+            <h1>
+              <span>Compétences</span>
+            </h1>
           </div>
           <div className="nine columns main-col">
-            <p>{skillmessage}
-            </p>
-				    <div className="skillz">
-					    {skills}
-				    </div>
-			    </div>
+            <p>{skillmessage}</p>
+            <div className="skillz">{skills}</div>
+          </div>
         </div>
-   </section>
+      </section>
     );
   }
 }
